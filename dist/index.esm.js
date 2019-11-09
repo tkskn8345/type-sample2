691579e474c1fe09baf094845d57ad292e052f31
kntks <0180takeshi.aefd@gmail.com>
2019-11-09 15:00:34 +0900

    add: buildで生成したdistディレクトリを追加

---
+++ dist/index.esm.js
@@ -0,0 +1,10 @@
+import { createElement, Fragment } from 'react';
+
+var Hello2 = function (_a) {
+    var text = _a.text;
+    return (createElement(Fragment, null,
+        createElement("h1", null, "\u3053\u3093\u306B\u3061\u306F"),
+        createElement("div", null, text)));
+};
+
+export default Hello2;
