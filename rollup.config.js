dfbc682f8086f916f28625c9aa8429505c706206
kntks <0180takeshi.aefd@gmail.com>
2019-11-09 20:04:15 +0900

    add: ローカルで開発するための環境を整える

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,34 @@
 import typescript from 'rollup-plugin-typescript2';
 import del from 'rollup-plugin-delete';
 import pkg from './package.json';
+import babel from 'rollup-plugin-babel';
 
 export default [
   {
     input: 'src/index.ts',
     output: [
-      {
-        file: 'playground/src/component-lib/index.js',
-        format: 'esm',
-        banner: '/* eslint-disable */',
-      },
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'esm' },
     ],
     plugins: [
-      del({ targets: ['dist/*', 'playground/src/component-lib'] }),
       typescript(),
     ],
     external: Object.keys(pkg.peerDependencies || {}),
   },
+  {
+    input: 'src/development.tsx',
+    output: [
+      {
+        file: 'development/development.js',
+        format: 'iife'
+      }
+    ],
+    plugins: [
+      typescript(),
+      babel({
+        exclude: 'node_modules/**',
+        presets: ['@babel/env', '@babel/preset-react'],
+      })
+    ],
+  }
 ];
---
+++ src/development.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Hello2 } from "./HelloWorld";
+
+ReactDOM.render(<Hello2 text="こんにちは" />, document.getElementById("root"));
